33d1f263bfd0bfc93780ef7e022be94cc6482754
georgesmyr <70004539+georgesmyr@users.noreply.github.com>
2025-01-05 03:48:01 +0100

    Added option to dump memory to file after execution.

--- sim8086/src/main.rs
+++ sim8086/src/main.rs
@@ -18,11 +18,14 @@ pub enum Command {
         path: PathBuf,
 
         #[arg(long, short)]
-        output: PathBuf,
+        output: Option<PathBuf>,
     },
 
     Execute {
         path: PathBuf,
+
+        #[arg(long, short)]
+        output: Option<PathBuf>,
     },
 }
 
@@ -35,10 +38,16 @@ fn main() -> Result<(), DecodeError> {
             let iqueue = decoder.decode()?;
             let asm = iqueue.to_string();
 
-            let mut output = fs::File::create(output).expect("Failed to create new output file.");
-            write!(output, "{asm}").expect("Failed to write output.");
+            match output {
+                Some(opath) => {
+                    let mut output =
+                        fs::File::create(opath).expect("Failed to create new output file.");
+                    write!(output, "{asm}").expect("Failed to write output.")
+                }
+                None => println!("{asm}"),
+            }
         }
-        Command::Execute { path } => {
+        Command::Execute { path, output } => {
             let buffer = fs::read(path).expect("Failed to read input byte-code file.");
             let decoder = Decoder::new(buffer);
             let iqueue = decoder.decode()?;
@@ -50,6 +59,11 @@ fn main() -> Result<(), DecodeError> {
             println!("-------------------");
             println!("{iqueue}");
             println!("{cpu}");
+
+            if let Some(opath) = output {
+                let mut file = fs::File::create(opath).expect("Failed to create new output file");
+                file.write_all(&cpu.mem).expect("Failed to write memory into output file");
+            }
         }
     }
     Ok(())
